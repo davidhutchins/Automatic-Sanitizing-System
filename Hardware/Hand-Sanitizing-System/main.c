dde8f8742ac5b8942b8438e6734a91860fc842a5
davidhutchins <davidhutchins@gmail.com>
2021-11-18 18:06:36 -0500

    Co-authored-by: Branden Evangelista <bkevangelista@users.noreply.github.com>

--- Hardware/Hand-Sanitizing-System/main.c
+++ Hardware/Hand-Sanitizing-System/main.c
@@ -1,36 +1,34 @@
-#include <msp430.h> 
+#include <msp430.h>
 #include <stdio.h>
 #include "sanitize.h"
 
-/**
- * main.c
- */
-
-void init_interrupts()
+void main(void)
 {
-    P1DIR |= 0x07; //(0000 0111) sets direction of pins 0-2 as output
-    P1OUT  = 0x30; //(0011 0000) configures Pins 4 and 5 as input
-    P1REN |= 0x30; //Adds pull up resistors to pins 4 and 5
-    P1IE  |= 0x30; //Enables interrupt on pins 4 and 5
-    P1IES |= 0x30; //Flag set with a high-to-low transition
-    P1IFG &= ~0x30; //Set interrupt on every pin except for 4 and 5
-
-    __bis_SR_register(LPM4_bits + GIE);     // Enable low power mode and enables GIE in status register for
-                                            // interrupts to be received
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+   
+    
+    
+    P3DIR |= BIT7;  // set pin 5 to output, rest to input
+    P1REN |= BIT4;
+    P1OUT |= BIT4;
+    P1IE  |= BIT4;  //Enables interrupt on pin 4
+    P1IES |= BIT4;  //Flag set with a low-to-high transition on all pins
+    LOCKLPM5
+    P1IFG &= ~0x10; // clearing the bit if it was somehow set
+    
+    __bis_SR_register(GIE);     // Enable low power mode and enables GIE in status register for
+                                // interrupts to be received
 }
 
 //ISR for Port 1
+
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1_interrupt(void)
 {
-
-}
-
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	init_interrupts();
-	
-
-	return 0;
+    P3OUT ^= BIT7;      // setting toggle bit 5
+    int i = 0;
+    while(i < 20000) {
+            i++;
+    }
+    P1IFG &= 0x00;     // clearing the interrupt flag
 }

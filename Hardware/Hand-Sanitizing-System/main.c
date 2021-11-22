43106386a43d52e51a61c82171325a6eeeee1783
davidhutchins <davidhutchins@gmail.com>
2021-11-22 15:56:19 -0500

    Interrupt and Sensor/ LED interface working. TODO: Timer Implementation

--- Hardware/Hand-Sanitizing-System/main.c
+++ Hardware/Hand-Sanitizing-System/main.c
@@ -1,34 +1,47 @@
 #include <msp430.h>
 #include <stdio.h>
 #include "sanitize.h"
+#include "msp430fr5994.h"
+#include "driverlib.h"
+
+/****** Initializing GPIO and Interrupts for main and safety mechanisms******/
+void gpio_init(void) {
+    P6DIR   = 0xFF;             // Set P1.0 as Output
+    P6OUT |= BIT1;              // Set BIT1 as high to start.
+
+    P1DIR   = 0x00;
+    P1IFG   &= ~(BIT2 | BIT4);  // Setting up main mechanism and safety
+    P1IES   |= BIT4;            // Bit 4 is high-to-low, so main mechanism
+    P1IES   &= ~BIT2;           // Bit 2 is low-to-high, so safety mechanism
+    P1IE    |= BIT2 | BIT4;     // Enabling both interrupts
+    P1REN   |= BIT4;            // Enabling pull up for main mechanism
+    P1OUT   |= BIT4;            // Setting it to pull up as opposed to pull down
+    PM5CTL0 &= ~LOCKLPM5;       // Unlocking LPM I/O pins
+
+}
 
 void main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
-   
-    
-    
-    P3DIR |= BIT7;  // set pin 5 to output, rest to input
-    P1REN |= BIT4;
-    P1OUT |= BIT4;
-    P1IE  |= BIT4;  //Enables interrupt on pin 4
-    P1IES |= BIT4;  //Flag set with a low-to-high transition on all pins
-    LOCKLPM5
-    P1IFG &= ~0x10; // clearing the bit if it was somehow set
-    
-    __bis_SR_register(GIE);     // Enable low power mode and enables GIE in status register for
-                                // interrupts to be received
+    gpio_init();
+    __bis_SR_register(GIE);
+    while(1);
+
+
 }
 
-//ISR for Port 1
 
+/****** Main/ Safety Mechanism Interrupt ******/
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1_interrupt(void)
 {
-    P3OUT ^= BIT7;      // setting toggle bit 5
-    int i = 0;
-    while(i < 20000) {
-            i++;
+    if (P1IFG & BIT2) {
+        P1IFG &= ~BIT2;     // clearing the interrupt flag
+        P6OUT &= ~BIT1;
+    } else if (P1IFG & BIT4) {
+        P1IFG &= ~BIT4;     // clearing the interrupt flag
+        P6OUT ^= BIT1;
+        P6OUT |= BIT2;
     }
-    P1IFG &= 0x00;     // clearing the interrupt flag
 }
+

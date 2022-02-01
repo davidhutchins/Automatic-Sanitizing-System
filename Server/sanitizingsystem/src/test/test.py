31c51e14b4f54cc8fedef16d05550bed6377d6be
Branden Evangelista <bevangelista@ufl.edu>
2022-02-01 11:37:12 -0500

    Add more mocks

--- Server/sanitizingsystem/src/test/test.py
+++ Server/sanitizingsystem/src/test/test.py
@@ -1,6 +1,7 @@
 import unittest
 import requests
 import json
+import copy
 
 url = "http://localhost:2000/"
 
@@ -37,6 +38,22 @@ class AppTest(unittest.TestCase):
         self.assertEqual(type(respJSON[0]['Thursday']), int)
         self.assertEqual(type(respJSON[0]['Friday']), int)
         self.assertEqual(type(respJSON[0]['Saturday']), int)
+    
+    def test_weekdata_get_content_read(self):
+        print("Testing/mocking that we can extract values from JSON (weekdata endpoint) onto a data array...")
+        dataArray = []
+        response = requests.get(url + 'weekdata')
+        respJSON = json.loads(response.content.decode("utf").replace("'", '"'))
+        
+        for key in respJSON[0]:
+            if (key != '_id'):
+                templateObject = {'day': key, 'sanitizations': respJSON[0][key]}
+                dataArray.append(templateObject)
+    
+        #Now loop through new data array
+        for i in range(len(dataArray)):
+            self.assertEqual(type(dataArray[i]['day']), str)
+            self.assertEqual(type(dataArray[i]['sanitizations']), int)
 
     # data endpoint (/data)
     def test_data_get_success(self):
@@ -45,7 +62,7 @@ class AppTest(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_data_get_content_size(self):
-        print("Testing that data JSON exists(i.e. has a size >= 0)...")
+        print("Testing that data JSON exists (i.e. has a size >= 0)...")
         response = requests.get(url + 'data')
         respJSON = json.loads(response.content.decode("utf").replace("'", '"'))
         self.assertGreaterEqual(len(respJSON), 0)
@@ -56,10 +73,29 @@ class AppTest(unittest.TestCase):
         respJSON = json.loads(response.content.decode("utf").replace("'", '"'))
         if (len(respJSON) > 0):
             for i in range(len(respJSON)):
-                pass
-
+                self.assertEqual(type(respJSON[i]["_id"]), str)
+                self.assertEqual(type(respJSON[i]["doorsSanid"]), int)
+                self.assertEqual(type(respJSON[i]["grmsKild"]), int)
         else:
             self.assertEqual(True, True)
+
+    def test_data_get_content_read(self):
+        print("Testing/mocking that we can extract values from JSON (data endpoint) onto a data array...")
+        dataArray = []
+        response = requests.get(url + 'data')
+        respJSON = json.loads(response.content.decode("utf").replace("'", '"'))
+        
+        if (len(respJSON) > 0):
+            for i in range(len(respJSON)):
+                templateObject = {'name': "Device " + str(i+1), 'value': respJSON[i]['doorsSanid']}
+                dataArray.append(templateObject)
+        else:
+            self.assertEqual(True, True)
+        
+        #Now loop through new data array
+        for i in range(len(dataArray)):
+            self.assertEqual(type(dataArray[i]['name']), str)
+            self.assertEqual(type(dataArray[i]['value']), int)
 
 if __name__ == '__main__':
     unittest.main()

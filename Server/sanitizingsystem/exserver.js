7f6d9fe9ffebaf2482b649db27bd37c8f29b9d3b
Bronte <brontecadogan@ufl.edu>
2022-02-02 23:05:04 -0500

    Updated some express routes / fixed package.json issue

--- Server/sanitizingsystem/exserver.js
+++ Server/sanitizingsystem/exserver.js
@@ -1,11 +1,22 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+const React = require ('react')
+var ReactDomServer = require('react-dom/server');
+const express = require('express'); 
+const app = express(); 
+const port = process.env.PORT || 5000; 
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+// port listening
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
+
+// sample get route
+app.get('/express_backend', (req, res) => { 
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+}); 
+
+
+
+app.get('/', (req, res) => { 
+  const component = ReactDomServer.renderToString("ss");
+  const html = `<h1> ${component}</h1>`;
+  res.send(html); 
+}); 

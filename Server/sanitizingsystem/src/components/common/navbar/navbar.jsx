b605e0a268063319dc2a4e9970b2672125af1862
Bronte <brontecadogan@ufl.edu>
2022-02-17 18:11:20 -0500

    authentication

--- Server/sanitizingsystem/src/components/common/navbar/navbar.jsx
+++ Server/sanitizingsystem/src/components/common/navbar/navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { getToken, removeUserSession, setUserSession } from './Common';
 import './navbar.css'
 import {
     BrowserRouter as Router,
@@ -9,7 +11,31 @@ import {
   import{Levels, Home, Stats, Data} from '../../common';
   import GUIDE from '../../../components/setup.pdf'
 
+  import Login from './Login';
+  import Dashboard from './Dashboard';
+
+
 function Navbar() {
+  const [authLoading, setAuthLoading] = useState(true);
+ 
+  useEffect(() => {
+    const token = getToken();
+    if (!token) {
+      return;
+    }
+ 
+    axios.get(`http://localhost:2000/verifyToken?token=${token}`).then(response => {
+      setUserSession(response.data.token, response.data.user);
+      setAuthLoading(false);
+    }).catch(error => {
+      removeUserSession();
+      setAuthLoading(false);
+    });
+  }, []);
+ 
+  if (authLoading && getToken()) {
+    return <div className="content">Checking Authentication...</div>
+  }
   return (
     <section id="router">
     <Router>
@@ -20,14 +46,11 @@ function Navbar() {
               <Link to="/" className="link">Home</Link>
             </li>
             <li>
+              <Link to="/login" className="link" id="login">Login</Link>
+            </li>
+            <li>
               <Link to="/stats" className="link">Sanitizing Statistics</Link>
             </li>
-            {/* <li>
-              <Link to="/levels"className="link">Power Levels</Link>
-            </li> */}
-            {/* <li>
-              <Link to="/data"className="link">Data</Link>
-            </li> */}
             <li>
               <Link to={GUIDE} target="_blank"className="link">Setup Guide</Link>
             </li>
@@ -38,9 +61,11 @@ function Navbar() {
       <Route path="/levels" element={<Levels/>}/>
       <Route path="/data" element={<Data/>}/>
       <Route path="/" element={<Home/>}/>
+      <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/guide" component={() => {window.location.href = {GUIDE} 
           return null;
         }}/>
+      <Route path="/login" element={<Login/>}/>
     </Routes>
       </div>
     </Router>

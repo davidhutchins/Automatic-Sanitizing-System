7f3d93cc3e3652419beaab4fab73e1e41f4793a7
Bronte <brontecadogan@ufl.edu>
2022-03-21 11:12:53 -0400

    fixed the font and added animation to Error Page

--- Server/sanitizingsystem/src/components/common/errorpage/ErrorPage.jsx
+++ Server/sanitizingsystem/src/components/common/errorpage/ErrorPage.jsx
@@ -6,12 +6,12 @@ function ErrorPage(props) {
     <section>
       <div id="errorMessage">
         <br />
-        Error! Not logged in!
+        <h1>Error! Not logged in!</h1>
         <br /><br />
       </div>
       <br /><br />
       <div id="directionMessage">
-        Please navigate back to the homepage to create an account and log into the website.
+        <h2>Please navigate back to the homepage to create an account and log into the website.</h2>
       </div>
     </section>
   );
